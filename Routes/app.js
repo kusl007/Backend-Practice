cba1ac876f7cdb5dd441bef3187f114b0f9c6096
kusl007 <kushal.bansal.1001@gmail.com>
2024-08-20 13:47:18 +0545

    routes added

---
+++ Routes/app.js
@@ -0,0 +1,12 @@
+const http=require("http");
+const server=http.createServer(function (req,res){
+    if (req.url==="/"){  res.end("hello")}
+    else if (req.url==="/profile"){  res.end("profile page")}
+    else{
+        res.end("page not found")
+    }
+    
+   
+
+})
+server.listen(3000)
