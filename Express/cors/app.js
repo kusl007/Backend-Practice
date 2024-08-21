5115f399dc6dbb6f321806ddeef39d795f761996
kusl007 <kushal.bansal.1001@gmail.com>
2024-08-21 16:27:01 +0545

    cookie parser added

--- Express/cors/app.js
+++ Express/cors/app.js
@@ -1,14 +1,27 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+var cookieParser = require('cookie-parser')
+app.use(cookieParser())
 
 // app.use(cors()) //it share all data
 
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
+
 app.get('/shareable',cors(), function (req, res) { // this shares limited data from that path only
   res.send('shareable data')
 })
 
+app.get('/banned', function (req, res,next) {
+    res.cookie('banned',"true")
+    res.cookie('name',"kushal")
+    res.send("banned")
+  })
+app.get('/check', function (req, res,next) {
+   console.log(req.cookies.banned)
+   console.log(req.cookies.name)
+   res.send("checking")
+  })
 app.listen(3000)
