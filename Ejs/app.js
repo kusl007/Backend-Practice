878cb963d896a9b581382588e7e9dc8908124c77
kusl007 <kushal.bansal.1001@gmail.com>
2024-08-22 13:25:34 +0545

    ejs form get added

--- Ejs/app.js
+++ Ejs/app.js
@@ -1,12 +1,21 @@
 const express = require('express')
 const app = express()
 app.set('view engine','ejs')
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
 app.get('/', function (req, res) {
   res.render('index')
 })
 app.get('/profile', function (req, res) {
   res.render('profile')
+})
+app.get('/check', function (req, res) {
+    console.log(req.query)
+  res.send('working ')
+})
+app.get('/form', function (req, res) {
+  res.render('form')
 })
 
 app.listen(3000)
