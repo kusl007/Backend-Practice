ae386fdbaa54d8fbf5d3ff2c193d67629f0044bb
kusl007 <kushal.bansal.1001@gmail.com>
2024-08-22 13:59:16 +0545

    error handling added with try catch

--- ErrorHandler/app.js
+++ ErrorHandler/app.js
@@ -2,12 +2,17 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send(Hello) 
+  try{
+    res.send(Hello) 
+  }
+  catch(err){
+    next(err)
+  }
 })
 app.get ("/hello",function(req,res){
     res.send("hello")
 })
 app.use((err,req,res,next)=>{
-    res.status(500).send("Internal Server Error")
+    res.status(500).send(err.message)
 })
 app.listen(3000)
