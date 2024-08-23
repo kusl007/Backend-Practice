36658fc0a2daf73c197de941abe9be8c138b7414
kusl007 <kushal.bansal.1001@gmail.com>
2024-08-23 14:14:45 +0545

    frontend connect and disconnect established

---
+++ webSockets/Connection_Frontend/app.js
@@ -0,0 +1,22 @@
+const express=require("express")
+const app = express();
+const socketIO=require("socket.io")
+const http=require("http")
+
+const server=http.createServer(app)
+const io=socketIO(server)
+
+app.set("view engine","ejs");
+
+io.on("connection",function(socket){
+  console.log("connected")  
+  socket.on("disconnect", function(){
+     console.log("disconnected")
+  })
+})
+
+app.get("/" ,(req,res)=>{
+    res.render("index")
+})
+
+server.listen(3000)
