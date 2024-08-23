7e0a1eb24c4136438462ec1d48ca7b15aced5947
kusl007 <kushal.bansal.1001@gmail.com>
2024-08-23 13:18:53 +0545

    server side and client side setup done

--- webSockets/app.js
+++ webSockets/app.js
@@ -5,8 +5,14 @@ const http=require("http")
 
 const server=http.createServer(app)
 const io=socketIO(server)
+
+app.set("view engine","ejs");
+
 io.on("connection",function(){
     console.log("connected")
 })
+app.get("/" ,(req,res)=>{
+    res.render("index")
+})
 
-app.listen(3000)
+server.listen(3000)
